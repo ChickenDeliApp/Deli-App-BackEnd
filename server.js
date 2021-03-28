a87e586be309829632c79aeebe206a70f845af47
Milosz <git@miloszmilewski.dev>
2021-03-28 22:44:51 +0100

    added Ciarans cor changes

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express")
+const cors = require("cors") 
+require("./database_conn")
 var app = express()
 
 app.use(express.urlencoded({extended: false}))
 
-
+app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+}) // Ciarans work
 
 const PORT = 3599 
 app.listen( PORT, () => {

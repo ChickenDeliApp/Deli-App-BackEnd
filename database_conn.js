c2827cc98c697fc379f9984bef91041a0cdb5ad9
Milosz <git@miloszmilewski.dev>
2021-03-28 22:45:15 +0100

    database model and connectivity

---
+++ database_conn.js
@@ -0,0 +1,143 @@
+require("dotenv").config()
+const { Sequelize, Model, DataTypes, DECIMAL, INTEGER } = require("sequelize")
+
+const sequelize = new Sequelize({
+    dialect: "mysql",
+    host: "localhost",
+    port: 3306,
+    database: "chicky",
+
+    username: "root",
+    password: "admin",
+})
+
+class User extends Model {}
+User.init({
+    username: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        unique: true
+    },
+
+    email: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        primaryKey: true,
+
+        validate: {
+            isEmail: {
+                msg: "Must be a valid e-mail"
+            }
+        }
+    },
+
+    password: {
+        type: DataTypes.STRING(60),
+        allowNull: false
+    },
+
+    isAdmin: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
+}, {
+    sequelize,
+
+    createdAt: "joinDate",
+    updatedAt: false,
+    timestamps: true
+})
+
+class DeliChain extends Model {}
+DeliChain.init({
+    chainName: {
+        type: DataTypes.STRING(80),
+        allowNull: false
+    },
+
+    // managedBy: {
+    //     type: DataTypes.STRING(255),
+
+    //     references: {
+    //         model: User,
+    //         key: "email"
+    //     }
+    // }
+}, {
+    sequelize,
+    updatedAt: false
+})
+User.hasOne(DeliChain, {foreignKey: "managedBy"})
+
+class DeliRestaurant extends Model {}
+DeliRestaurant.init({
+    restaurantName: {
+        type: DataTypes.STRING(80)
+    },
+
+    // restaurantChain: {
+    //     type: DataTypes.INTEGER,
+
+    //     references: {
+    //         model: DeliChain,
+    //         key: "id"
+    //     }
+    // },
+
+    geo_location: {
+        type: DataTypes.GEOMETRY("POINT")
+    },
+
+    address: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    postCode: {
+        type: DataTypes.STRING(10),
+        allowNull: false
+    }
+}, {
+    sequelize,
+    updatedAt: false
+})
+DeliChain.hasMany(DeliRestaurant, {foreignKey: "restaurantChain"})
+
+class Review extends Model {}
+Review.init({
+    text: {
+        type: DataTypes.STRING(500)
+    },
+    rating: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            max: 5,
+            min: 1,
+            notNull: true
+        }
+    }
+}, {
+    sequelize,
+    updatedAt: "lastUpdated",
+    createdAt: "createDate"
+})
+DeliRestaurant.hasMany(Review, {foreignKey: "restaurant"})
+User.hasOne(Review, {foreignKey: "createdBy"})
+
+const DEV = process.env.DEV || false
+if(DEV){
+    sequelize.query('SET FOREIGN_KEY_CHECKS = 0').then(async () => {
+        await sequelize.sync({force: true});
+        await sequelize.query("SET FOREIGN_KEY_CHECKS = 1")
+    })
+}
+
+
+module.exports = {
+    sequelize,
+    User,
+    Review,
+    DeliChain,
+    DeliRestaurant
+}

b617acf74aa030be316e9146f816e1cec115aabe
Milosz <git@miloszmilewski.dev>
2021-04-21 12:59:29 +0100

    Point hack for sequelize

--- database_conn.js
+++ database_conn.js
@@ -2,6 +2,21 @@ require("dotenv").config()
 const { Sequelize, Model, DataTypes, DECIMAL, INTEGER } = require("sequelize")
 const bcrypt = require("bcrypt")
 
+// https://github.com/sequelize/sequelize/issues/9786
+const wkx = require('wkx')
+Sequelize.GEOMETRY.prototype._stringify = function _stringify(value, options) {
+  return `ST_GeomFromText(${options.escape(wkx.Geometry.parseGeoJSON(value).toWkt())})`;
+}
+Sequelize.GEOMETRY.prototype._bindParam = function _bindParam(value, options) {
+  return `ST_GeomFromText(${options.bindParam(wkx.Geometry.parseGeoJSON(value).toWkt())})`;
+}
+Sequelize.GEOGRAPHY.prototype._stringify = function _stringify(value, options) {
+  return `ST_GeomFromText(${options.escape(wkx.Geometry.parseGeoJSON(value).toWkt())})`;
+}
+Sequelize.GEOGRAPHY.prototype._bindParam = function _bindParam(value, options) {
+  return `ST_GeomFromText(${options.bindParam(wkx.Geometry.parseGeoJSON(value).toWkt())})`;
+}
+
 const sequelize = new Sequelize({
     dialect: "mysql",
     host: process.env.HOST || "localhost",
